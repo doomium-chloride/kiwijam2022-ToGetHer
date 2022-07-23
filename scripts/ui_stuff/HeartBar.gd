011da1ee27714d91e3d1382cb2ead23715f6e88a
Jonathan Sulinom <JSulinom@flintfox.com>
2022-07-23 20:54:07 +1200

    Add healthbar

---
+++ scripts/ui_stuff/HeartBar.gd
@@ -0,0 +1,38 @@
+extends Control
+
+const maxValue = 20
+export var value = 20
+
+var hearts : Array = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initHeartsArray()
+	update()
+
+func initHeartsArray():
+	# Clear array
+	hearts = []
+	# top layer
+	hearts.append($TopLayer/Heart01)
+	hearts.append($TopLayer/Heart02)
+	hearts.append($TopLayer/Heart03)
+	hearts.append($TopLayer/Heart04)
+	hearts.append($TopLayer/Heart05)
+	# bottom layer
+	hearts.append($BottomLayer/Heart01)
+	hearts.append($BottomLayer/Heart02)
+	hearts.append($BottomLayer/Heart03)
+	hearts.append($BottomLayer/Heart04)
+	hearts.append($BottomLayer/Heart05)
+
+func update():
+	var hp = value
+	for heart in hearts:
+		if hp >= 2:
+			heart.setFull()
+		elif hp == 1:
+			heart.setHalf()
+		else:
+			heart.setEmpty()
+		hp = max(0, hp-2)
