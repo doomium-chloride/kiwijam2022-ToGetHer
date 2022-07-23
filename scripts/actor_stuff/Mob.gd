fb56445cc4d149bc2a8683a2b47e829896ee3e2a
Jonathan Sulinom <JSulinom@flintfox.com>
2022-07-23 18:39:44 +1200

    Add has line of sight

--- scripts/actor_stuff/Mob.gd
+++ scripts/actor_stuff/Mob.gd
@@ -1,6 +1,9 @@
 extends "res://scripts/actor_stuff/Actor.gd"
 
-var isHostile = false
+export var alwaysHostile = false
+export var sightRange = 10
+export var aggroLevel = 0
+export var maxAggroLevel = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -22,6 +25,31 @@ func randomWalk():
 		
 func getDir():
 	return randomWalk()
+
+func decayAggro():
+	aggroLevel = int(max(0, aggroLevel - 1))
+
+func gainAggro():
+	aggroLevel = maxAggroLevel
+
+func isHostile():
+	return aggroLevel > 0
+	
+func withinSightRangeOfPlayer(playerCoord : Vector2):
+	var distance = position.distance_to(playerCoord)
+	return distance <= sightRange
+	
+func canSeePlayer(playerCoord : Vector2):
+	var withintRange = withinSightRangeOfPlayer(playerCoord)
+	if withintRange:
+		return PathFinding.hasLineOfSight(coords(), playerCoord, GridManager.tilemap)
+	else:
+		return false
+
+# call this method to get the Mob to execute its turn
+func executeTurn():
+	# TODO: pass appropriate paramters
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
